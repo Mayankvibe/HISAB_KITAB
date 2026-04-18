4a66bce025b5bb7086412581b6ed70e30405a7d2
MAYANK SAHU <harnarayansahu617@gmail.com>
2026-04-18 15:59:49 +0530

    modified file added

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-from fastapi import FastAPI, Request,Form
-from fastapi.templating import Jinja2Templates 
-
+from fastapi import FastAPI
+from routes import auth
 app=FastAPI()
-templates=Jinja2Templates(directory="templates")
-
-@app.get("/")
-def intro(request:Request):
-    return templates.TemplateResponse("index.html",{"request":request})
-
-@app.get("/signup")
-def signup_page(request:Request):
-    return templates.TemplateResponse("signup.html",{"request":request})
-
-users={}
-@app.post("/signup", status_code=201,)
-def signup(phone: str = Form(...), password: str = Form(...)):
-    users[phone] = password
-    return templates.TemplateResponse("signup.html",{"request":request , "msg":"done signup "})
-
-@app.post("/login", status_code=201)
-def create(phone: str = Form(...), password: str = Form(...)):
-    pass
+app.include_router(auth.routes,prefix="/auth")
