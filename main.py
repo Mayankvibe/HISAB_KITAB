b0796b950e6fa054fe5b022e1d85d6714254cae5
MAYANK SAHU <harnarayansahu617@gmail.com>
2026-05-24 14:32:30 +0530

    modified

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from fastapi import FastAPI
-from routes import auth
+from routes import auth,base_dashboard
+from fastapi.staticfiles import StaticFiles
+import model
+from database import engine
 app=FastAPI()
+
+model.Base.metadata.create_all(
+    bind=engine
+)
+
 app.include_router(auth.routes,prefix="/auth")
+app.include_router(base_dashboard.routes,prefix="/base_dashboard")
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
