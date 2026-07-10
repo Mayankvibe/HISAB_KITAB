b968d8cdea0dafbfd7efecc1f69497e47485f3c5
MAYANK SAHU <harnarayansahu617@gmail.com>
2026-07-10 11:00:52 +0530

    modified

---
+++ auth_jwt.py
@@ -0,0 +1,27 @@
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+security=HTTPBearer()
+SECRET_KEY = "mysecretkey"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return token
+
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token=credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+       raise HTTPException(status_code=401,detail="invaild token")
